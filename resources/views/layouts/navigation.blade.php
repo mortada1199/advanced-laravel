cc84de322ab71b22e0b50a5d44729777d465031f
mortada1199 <murtada199815@gmail.com>
2024-12-17 13:34:37 +0200

    navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,10 +16,10 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
                         Schedule a Class
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
                         Upcoming Classes
                     </x-nav-link>
 
@@ -86,6 +86,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+
+            
         </div>
 
         <!-- Responsive Settings Options -->
