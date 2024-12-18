503b6baa38a515be402d2aada8b7746c0c420cb9
mortada1199 <murtada199815@gmail.com>
2024-12-18 16:04:35 +0200

    schedula class Schedula

--- app/Models/ScheduledClass.php
+++ app/Models/ScheduledClass.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ScheduledClass extends Model
 {
@@ -15,20 +17,33 @@ class ScheduledClass extends Model
         'date_time' => 'datetime'
     ];
 
-public function instructor()
-{
-    return $this->belongsTo(User::class ,'instructor_id');
-}
+    public function instructor()
+    {
+        return $this->belongsTo(User::class, 'instructor_id');
+    }
 
 
-public function ClassType()
-{
-    return $this->belongsTo(ClassType::class);
-}
+    public function ClassType()
+    {
+        return $this->belongsTo(ClassType::class);
+    }
 
 
-public function members(){
-    return $this->belongsToMany(User::class,'bookings');
-}
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'bookings');
+    }
+
+
+    public function scopeUpcoming(Builder $query)
+    {
+        return $query->where('date_time', '>', now());
+    }
+
+    public function scopeNotBooked(Builder $query) {
+        return $query->whereDoesntHave('members', function($query) {
+            $query->where('user_id', auth()->user()->id);
+        });
+    }
 
 }
